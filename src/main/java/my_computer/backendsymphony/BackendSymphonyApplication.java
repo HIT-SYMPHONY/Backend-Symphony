403260953c8c9dc33ffe545fd63eceaf8fcce2a2
khuongtoan <kxtdumbo@gmail.com>
2025-06-16 19:18:46 +0700

    Init package

---
+++ src/main/java/my_computer/backendsymphony/BackendSymphonyApplication.java
@@ -0,0 +1,13 @@
+package my_computer.backendsymphony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSymphonyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSymphonyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my_computer/backendsymphony/BackendSymphonyApplicationTests.java
@@ -0,0 +1,13 @@
+package my_computer.backendsymphony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSymphonyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
