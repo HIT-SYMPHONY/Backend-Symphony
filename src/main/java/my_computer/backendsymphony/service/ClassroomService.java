7de1865ee0c947fa5b398eb3424e2823e41efab6
txt1stparkuor <167858766+txt1stparkuor@users.noreply.github.com>
2025-06-29 12:00:45 +0700

    feat: add update classroom api (#29)
    * feat: add update classroom api
    
    * refactor: move preAuthorize to controller
    
    * fix: remove @.repository in serivce
    
    * fix: fix update classroom api authorization

--- src/main/java/my_computer/backendsymphony/service/ClassroomService.java
+++ src/main/java/my_computer/backendsymphony/service/ClassroomService.java
@@ -1,11 +1,19 @@
 package my_computer.backendsymphony.service;
 
 import my_computer.backendsymphony.domain.dto.request.ClassroomCreationRequest;
+import my_computer.backendsymphony.domain.dto.request.ClassroomUpdateRequest;
 import my_computer.backendsymphony.domain.dto.response.ClassroomResponse;
+
+import my_computer.backendsymphony.domain.entity.ClassRoom;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ClassroomService {
     ClassroomResponse createClassroom(ClassroomCreationRequest request, MultipartFile imageFile);
 
     void deleteClassroom(String id);
+
+    ClassroomResponse updateClassroom(String id, ClassroomUpdateRequest request,MultipartFile imageFile);
+
+    boolean isLeaderOfClassroom(ClassRoom classRoom, Authentication authentication);
 }
