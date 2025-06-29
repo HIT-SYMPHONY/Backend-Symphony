d5a4f6de1c14d9c0ea8c7921864e8cfc395303c6
txt1stparkuor <txt1stparkuor@gmail.com>
2025-06-29 10:25:11 +0700

    fix: remove @.repository in serivce

--- src/main/java/my_computer/backendsymphony/service/ClassroomService.java
+++ src/main/java/my_computer/backendsymphony/service/ClassroomService.java
@@ -3,10 +3,9 @@ package my_computer.backendsymphony.service;
 import my_computer.backendsymphony.domain.dto.request.ClassroomCreationRequest;
 import my_computer.backendsymphony.domain.dto.request.ClassroomUpdateRequest;
 import my_computer.backendsymphony.domain.dto.response.ClassroomResponse;
-import org.springframework.stereotype.Repository;
+
 import org.springframework.web.multipart.MultipartFile;
 
-@Repository
 public interface ClassroomService {
     ClassroomResponse createClassroom(ClassroomCreationRequest request, MultipartFile imageFile);
 
