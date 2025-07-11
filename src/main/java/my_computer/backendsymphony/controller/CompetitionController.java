88ee06546c3dc989d62ca736a09ebfb050730d7f
txt1stparkuor <txt1stparkuor@gmail.com>
2025-07-11 20:23:35 +0700

    feat: add get competition by id api

--- src/main/java/my_computer/backendsymphony/controller/CompetitionController.java
+++ src/main/java/my_computer/backendsymphony/controller/CompetitionController.java
@@ -11,6 +11,8 @@ import my_computer.backendsymphony.domain.dto.request.CompetitionCreationRequest
 import my_computer.backendsymphony.service.CompetitionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,5 +29,10 @@ public class CompetitionController {
             @Valid @RequestPart("data") CompetitionCreationRequest request,
             @RequestPart(value = "image", required = false) MultipartFile imageFile) {
         return VsResponseUtil.success(competitionService.createCompetition(request, imageFile));
+    }
+
+    @GetMapping(UrlConstant.Competition.COMPETITION_ID)
+    public ResponseEntity<?> getCompetitionById(@PathVariable String id) {
+        return VsResponseUtil.success(competitionService.getCompetitionById(id));
     }
 }
