5b58ba102b04cd9639ada0ce52c88500879f3b9d
Phạm Đức Thành <thnhpahm2710@gmail.com>
2025-07-12 20:37:34 +0700

    feat: get lessons by classroom id and by current user id (#44)

--- src/main/java/my_computer/backendsymphony/constant/UrlConstant.java
+++ src/main/java/my_computer/backendsymphony/constant/UrlConstant.java
@@ -26,6 +26,8 @@ public class UrlConstant {
         public static final String CREATE_LESSON = PRE_FIX;
         public static final String DELETE_LESSON = PRE_FIX+"/{lessonId}";
         public static final String UPDATE_LESSON = PRE_FIX+"/{lessonId}";
+        public static final String GET_LESSON_BY_CLASSROOM_ID = PRE_FIX + "/classroom/{classroomId}";
+        public static final String GET_LESSON_BY_CURRENT_USER_ID = PRE_FIX + "/my-lessons";
         private Lesson(){
         }
     }
