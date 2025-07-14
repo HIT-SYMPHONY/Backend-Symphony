e11461615e0bfbfde8ed2db721d152780d692c40
Khương Xuân Toàn <kxtdumbo@gmail.com>
2025-07-14 12:17:24 +0700

    get all comment of a post (#62)

--- src/main/java/my_computer/backendsymphony/service/CommentPostService.java
+++ src/main/java/my_computer/backendsymphony/service/CommentPostService.java
@@ -3,12 +3,14 @@ package my_computer.backendsymphony.service;
 import my_computer.backendsymphony.domain.dto.request.CommentPostRequest;
 import my_computer.backendsymphony.domain.dto.response.CommentPostResponse;
 
+import java.util.List;
+
 public interface CommentPostService {
 
     CommentPostResponse createCommentPost (CommentPostRequest commentPostRequest);
 
     CommentPostResponse deleteCommentPost (String commentPostId);
 
-    CommentPostResponse getCommentPostById (String commentPostId);
+    List<CommentPostResponse> getCommentPostByPostId (String PostId);
 
 }
