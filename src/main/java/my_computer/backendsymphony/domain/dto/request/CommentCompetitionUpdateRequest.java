2eca545c5dbee212c17f98dca023eb633434c773
txt1stparkuor <txt1stparkuor@gmail.com>
2026-03-24 22:14:07 +0700

    feat: add competition user related api

--- src/main/java/my_computer/backendsymphony/domain/dto/request/CommentCompetitionUpdateRequest.java
+++ src/main/java/my_computer/backendsymphony/domain/dto/request/CommentCompetitionUpdateRequest.java
@@ -1,19 +1,13 @@
 package my_computer.backendsymphony.domain.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import my_computer.backendsymphony.constant.ErrorMessage;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
-public class JoinCompetitionRequest {
-
+public class CommentCompetitionUpdateRequest {
     @NotBlank(message = ErrorMessage.Validation.NOT_BLANK)
-    private String competitionId;
-
+    private String content;
 }
---
+++ src/main/java/my_computer/backendsymphony/domain/dto/request/CompetitionUserUpdateRequest.java
@@ -0,0 +1,4 @@
+package my_computer.backendsymphony.domain.dto.request;
+
+public class CompetitionUserUpdateRequest {
+}
